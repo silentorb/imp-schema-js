0dc6d4c8f76f8a73fa061f689ec539e17b274f36
silentorb <christopherwjohnson@gmail.com>
2015-07-18 16:06:41 -0600

    Minor tweaks.

--- make.bat
+++ make.bat
@@ -1,1 +1,3 @@
-start "" E:\Dev\imp\Imp_CLI\bin\Debug\imp-cli.exe -i lib -o imp-schema.js -t js
+start "" E:\Dev\imp\Imp_CLI\bin\Debug\imp-cli.exe -i node_modules/imp-schema/lib -o node_modules/imp-schema/imp-schema.js -t js
+
+imp -i lib -o imp-schema.js -t js
